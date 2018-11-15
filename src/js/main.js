4b932c7bc5937380c1b829bab5a8d49035cdf148
Laurens Kalf <laurenskalf@Sattes-iMac.local>
2018-11-15 15:22:34 +0100

    Style changes, mainly to the left-hand side nav menu

--- src/js/main.js
+++ src/js/main.js
@@ -37,6 +37,7 @@
 
  });
 
+
  // Code to show and hide scroll-to-top button
  // When the user scrolls down 400px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
